f4e40832a0550f287e6e7d748dedfe8c33c8c32f
VickyMutai <vicky.mutai96@gmail.com>
2018-02-15 18:12:58 +0300

    create register form

--- app/auth/views.py
+++ app/auth/views.py
@@ -8,4 +8,12 @@ from . import auth
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        community = Community
+        community = Community(username = form.username.data,
+                              email = form.email.data,
+                              password = form.password.data
+                             )
+        db.session.add(community)
+        db.session.commit()
+        return redirect(url_for('auth.login'))
+        title =  'Create Account'
+    return render_template('auth/register.html',registration_form = form)
